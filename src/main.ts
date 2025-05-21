86877f0bf1a18f3c899609cdd1c5e47cc2d7a0ef
Willian Rodrigues <willian.rsilva1994@gmail.com>
2025-05-21 13:06:21 -0300

    [feat] - Implements main base config

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,48 @@
+import { ValidationPipe, VersioningType } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import * as bodyParser from 'body-parser';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  app.use(bodyParser.json({ limit: '4.5mb' }));
+  app.use(bodyParser.urlencoded({ limit: '4.5mb', extended: true }));
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
+
+  app.enableCors({
+    credentials: true,
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+    origin: [process.env.CLIENT_URL],
+    allowedHeaders: [
+      'X-Requested-With',
+      'X-HTTP-Method-Override',
+      'Content-Type',
+      'Accept',
+      'Observe',
+      'X-Authorization',
+      'X-Token-Auth',
+      'Authorization',
+    ],
+    methods: 'GET, POST, PUT, DELETE',
+  });
+
+  app.enableVersioning({
+    type: VersioningType.URI,
+    defaultVersion: '1',
+  });
+
+  app.setGlobalPrefix('api');
+
+  await app.listen(process.env.PORT || 3333);
 }
-bootstrap();
+
+void bootstrap();
