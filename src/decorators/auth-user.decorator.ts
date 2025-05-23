b44c8966d5805f58e1ba4216b9f4585013dcb9c1
Willian Rodrigues <willian.rsilva1994@gmail.com>
2025-05-22 22:13:47 -0300

    [fix] - authUser decorator

--- src/decorators/auth-user.decorator.ts
+++ src/decorators/auth-user.decorator.ts
@@ -1,11 +1,11 @@
 import { ExecutionContext, createParamDecorator } from '@nestjs/common';
+import { plainToInstance } from 'class-transformer';
 import { User } from 'src/@types/user.type';
-import { defaultPlainToClass } from 'src/utils/default-plain-to-class.util';
 
 export const AuthUser = createParamDecorator(
   (_data, ctx: ExecutionContext): User => {
     const request = ctx.switchToHttp().getRequest();
 
-    return defaultPlainToClass(User, request.user);
+    return plainToInstance(User, request.user);
   },
 );
